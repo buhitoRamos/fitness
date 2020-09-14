8a76a6b6016a15b53e12af2a39f6dd4da79230a9
Martin Lopez <martin.lopezx@neoris.com>
2020-09-14 20:59:20 -0300

    Se corrigieron estilos y trabajando con estilo en linea + css + boostraps

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,22 +1,28 @@
 import React from 'react'
-import exerciseImg from '../images/exercise.png'
+import circleImg from '../images/circles.png'
 import './styles/Card.css'
 class Card extends React.Component {
-    render(){
+ render(){
+     //creando esta constante evito usar this.props.img==quedando mas corto
+     const {title, description, rightColour, leftColour, img } =this.props
         return (
-            <div className="card mx-auto Fitness-Card">
-                 <div className="card-body">
-                      <div className="row center">
-                           <div className= "col-6">
-                               <img src={exerciseImg} className="float-rigth"/>
-                         </div>
-                          <div className="col-6 Fitness-Card-Info">
-                            <h1> Techniques Guides</h1>
-                            <p> Learn amazing street and calisthenics</p>
-                          </div>
-                       </div>
-                  </div>
-              </div>  
+            <div className="card mx-auto Fitness-Card"
+            style={{
+                backgroundImage: `url(${circleImg}), linear-gradient(to right, ${rightColour}, ${leftColour})`
+            }}
+            >
+                <div className="card-body">
+                    <div className="row center">
+                        <div className="col-6">
+                            <img src={img} className="float-right"/>
+                        </div> 
+                        <div className="col-6 Fitness-Card-Info">
+                            <h1>{title}</h1>
+                            <p>{description}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         )
     }
 }
--- src/index.js
+++ src/index.js
@@ -16,4 +16,10 @@ import 'bootstrap/dist/css/bootstrap.css'
 
 const container =document.getElementById('root')
 
- ReactDOM.render(<Card />, container)
+ ReactDOM.render(<Card
+                        title="Technique Guides"
+                        description="Learn amazing street workout and calisthenics"
+                        rightColour="#A74CF2"
+                        leftColour="617BFB" 
+                        img="https://firebasestorage.googleapis.com/v0/b/tutoriales-e4830.appspot.com/o/exercise.png?alt=media&token=b9c4b236-16a9-4a56-bba2-90c9660a0f06"
+                        />, container)
