50f86629e3b1e83dabef47e1d3098ca443aae251
buhito <cym.martin85@gmail.com>
2020-09-16 00:18:20 -0300

    Utilizando state para asignar valor y editar variables, tambien funcion componentDidMount

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,9 +2,30 @@ import React from 'react'
 import circleImg from '../images/circles.png'
 import './styles/Card.css'
 class Card extends React.Component {
+
+    constructor(props){
+        super(props)
+        this.state={
+           image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQMoJ5SbQWvDZmNQoqu24DGDZuyLdPd3_UflA&usqp=CAU'
+        }
+
+    }
+    componentDidMount(){
+        setTimeout(()=> {
+            this.setState({
+                image: this.props.img
+            })
+        },5000)
+       
+    }
+
  render(){
-     //creando esta constante evito usar this.props.img==quedando mas corto
-     const {title, description, rightColour, leftColour, img } =this.props
+   //creando esta constante "const {x,x}=this.props" evito usar this.props.img==quedando mas corto
+  //las props siempre son heredadas del padre y son contantes
+ //el estate son las variables, y siempre se definenn en el constructor
+//componenDidMount petenece al ciclo de vida de un componente en reac, y es un evento que se inicia cuando carga el componente
+    
+     const {title, description, rightColour, leftColour,img } =this.props
         return (
             <div className="card mx-auto Fitness-Card"
             style={{
@@ -14,7 +35,7 @@ class Card extends React.Component {
                 <div className="card-body">
                     <div className="row center">
                         <div className="col-6">
-                            <img src={img} className="float-right"/>
+                            <img src={this.state.image} className="float-right"/>
                         </div> 
                         <div className="col-6 Fitness-Card-Info">
                             <h1>{title}</h1>
