99c7b72b4ff233b6575bbfd2e435c04ff57d9756
Martin Lopez <martin.lopezx@neoris.com>
2020-09-20 22:34:32 -0300

    Se cambio el BtnNavegation a componente funcional, y se agregó browserRouter al proyecto

--- src/components/BtnNavegation.js
+++ src/components/BtnNavegation.js
@@ -1,21 +1,23 @@
 import React from 'react'
 import './styles/Btn.css'
+import btn from '../images/btn.png'
 
-class BtnNavegation extends React.Component {
-    constructor(props){
-        super(props)
-        this.state={
-            img:'https://firebasestorage.googleapis.com/v0/b/tutoriales-e4830.appspot.com/o/add.png?alt=media&token=fbe1d973-4b4f-42d8-b76e-2c4354f6ed9d'
-        }
-    }
-render(){
+function BtnNavegation (props)  {
+   
+
     return(
         
     <div>
-    <img src={this.state.img} className="btn" />
+        <a href="/exercises/new">
+    <img src={btn} className="btn" />
+    </a>
+
    </div>
     )
     
 }
-}
+
 export default BtnNavegation
+/*
+Este componente es el boton de navegación 
+*/
