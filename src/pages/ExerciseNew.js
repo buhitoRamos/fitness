435eceb38be644152f5f3c0250932422d7390271
Martin Lopez <martin.lopezx@neoris.com>
2020-09-24 15:26:55 -0300

    Manejo de Eventos en React

--- src/pages/ExerciseNew.js
+++ src/pages/ExerciseNew.js
@@ -3,20 +3,100 @@ import BtnNavegation from '../components/BtnNavegation'
 
 class ExerciseNew extends React.Component{
 
-    render(){
-        return(
-            <div>
-                <h1>hola soy un fucking h1
-                    ......por ahora......
-                </h1>
-                <div>
-                <BtnNavegation
-                url="/exercisesx"
-                />
-                </div>
-            </div>
-        )
+    /*
+    Se inicializa el estate como obeto vacío para que no de error
+    se puede utilizar clases por por la ayuda de BABEL estamos trabajando con ES7
+    */
+
+    state = {}
+
+    //e.preventDefault es para que no refresque la pagina al hacer submit
+
+    handleSubmit = e => {
+        e.preventDefault()
+        console.log(this.state)
     }
 
+    /*
+    Este Evento handleChange(onChange) cada ves que se da un cambio
+     (presiono una tecla) captura el evento con el nombre del input y valor
+    y lo guarda en el state
+    */
+
+    handleChange = e => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+
+    render(){
+        return (
+            <div className="container">
+            <form 
+                onSubmit={this.handleSubmit}
+            >
+                <div className="form-group">
+                    <input 
+                        type="text" 
+                        className="form-control" 
+                        placeholder="title" 
+                        name="title"
+                        onChange={this.handleChange}
+                        value={this.state.title}
+                    />
+                </div>
+                <div className="form-group">
+                    <input 
+                        type="text" 
+                        className="form-control" 
+                        placeholder="description" 
+                        name="description"
+                        onChange={this.handleChange}
+                        value={this.state.description}
+                    />
+                </div>
+                <div className="form-group">
+                    <input 
+                        type="text" 
+                        className="form-control" 
+                        placeholder="img" 
+                        name="img"
+                        onChange={this.handleChange}
+                        value={this.state.img}
+                    />
+                </div>
+                <div className="form-row">
+                    <div className="col">
+                        <input 
+                            type="text" 
+                            className="form-control" 
+                            placeholder="leftColor" 
+                            name="leftColor"
+                            onChange={this.handleChange}
+                            value={this.state.leftColor}
+                        />
+                    </div>
+                    <div className="col">
+                        <input 
+                            type="text" 
+                            className="form-control"
+                            placeholder="rightColor" 
+                            name="rightColor"
+                            onChange={this.handleChange}
+                            value={this.state.rightColor}
+                        />    
+                    </div>
+                </div>
+                
+                <button 
+                    type="submit" 
+                    className="btn btn-primary"
+                >
+                    Submit
+                </button>
+            </form>
+        </div>
+        )
+    }
 }
  export default ExerciseNew
